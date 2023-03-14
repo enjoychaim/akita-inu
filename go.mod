512046c76bb3450855825560df464fb8201ec219
chaim <chenming@shoplazza.com>
2023-03-14 16:20:05 +0800

    add json pretty

--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/sirupsen/logrus v1.8.1
 	github.com/smartystreets/goconvey v1.7.2
 	github.com/stretchr/testify v1.7.0
+	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/urfave/cli v1.22.5
 	go.uber.org/zap v1.19.1
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
--- json/json.go
+++ json/json.go
@@ -1,8 +1,17 @@
 package json
 
-import "github.com/json-iterator/go"
+import (
+	"fmt"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/tidwall/pretty"
+)
 
 var (
 	JJ     = jsoniter.ConfigCompatibleWithStandardLibrary // 100% compatible with standard library behavior
 	FastJJ = jsoniter.ConfigFastest                       // ConfigFastest marshals float with only 6 digits precision
 )
+
+func Pretty(msg string) string {
+	return fmt.Sprintf("%s", pretty.Pretty([]byte(msg)))
+}
