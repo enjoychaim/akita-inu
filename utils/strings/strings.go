715cd52b4d55c9460c14e1de4f93fee4b5acc963
chaim <chenming@shoplazza.com>
2023-03-22 14:21:21 +0800

    add ToCapitalized

--- utils/strings/strings.go
+++ utils/strings/strings.go
@@ -9,3 +9,11 @@ func ToCamelCase(s string, step string) string {
 	}
 	return strings.Join(words, "")
 }
+
+func ToCapitalized(s string, step string) string {
+	words := strings.Split(s, step)
+	for i, word := range words {
+		words[i] = strings.Title(strings.ToLower(word))
+	}
+	return strings.Join(words, step)
+}
