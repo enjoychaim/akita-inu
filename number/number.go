7c68b0a4f0511f4ee494d259c68ea2fe37c44384
chaim <chenming@shoplazza.com>
2023-04-14 10:27:32 +0800

    add number

---
+++ number/number.go
@@ -0,0 +1,15 @@
+package int
+
+import (
+	"fmt"
+)
+
+func FormatNumber(n float64) string {
+	if n < 1000 {
+		return fmt.Sprintf("%f", n)
+	}
+	if n < 1000000 {
+		return fmt.Sprintf("%.1fK", float64(n)/1000)
+	}
+	return fmt.Sprintf("%.1fM", float64(n)/1000000)
+}
