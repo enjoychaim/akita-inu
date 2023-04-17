bdd19ad819fae9f9f4913d39c1e858a56e46ec6c
chaim <chenming@shoplazza.com>
2023-04-17 14:19:22 +0800

    number  format

--- number/number.go
+++ number/number.go
@@ -6,10 +6,13 @@ import (
 
 func FormatNumber(n float64) string {
 	if n < 1000 {
-		return fmt.Sprintf("%f", n)
+		return fmt.Sprintf("%.0f", n)
 	}
 	if n < 1000000 {
-		return fmt.Sprintf("%.1fK", float64(n)/1000)
+		return fmt.Sprintf("%.0fK", float64(n)/1000)
 	}
-	return fmt.Sprintf("%.1fM", float64(n)/1000000)
+	if n < 1000000000 {
+		return fmt.Sprintf("%.0fM", float64(n)/1000000)
+	}
+	return fmt.Sprintf("%.0fB", float64(n)/1000000000)
 }
